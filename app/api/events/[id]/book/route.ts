142500019f309d526f67975c2bc080e10e066650
Coreyne <corenegasore@gmail.com>
2024-11-01 20:07:48 +0200

    fix: types error

--- app/api/events/[id]/book/route.ts
+++ app/api/events/[id]/book/route.ts
@@ -1,27 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server'
 import { bookEvent } from '@/lib/db'
 
 export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const id = params.id;
+  {}
+  const { id } =await params;
   
   try {
     const body = await request.json();
     const updatedEvent = await bookEvent(id, body);
 
     if (updatedEvent) {
-      return NextResponse.json(updatedEvent);
+      return Response.json(updatedEvent);
     } else {
-      return NextResponse.json(
+      return Response.json(
         { error: 'No available seats or event not found' }, 
         { status: 400 }
       );
     }
   } catch (error) {
     console.error('Error booking event:', error);
-    return NextResponse.json(
+    return Response.json(
       { error: 'An error occurred while booking the event' }, 
       { status: 500 }
     );
